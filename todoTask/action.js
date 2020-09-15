5d16dbfa11802ed5596615978dc78729dfb4fb6b
serjG901 <70537823+serjG901@users.noreply.github.com>
2020-09-15 15:47:52 +0300

    Update action.js

--- todoTask/action.js
+++ todoTask/action.js
@@ -1,11 +1,43 @@
-﻿let checked = document.getElementById("checked");
-
-checked.addEventListener("change", () => {
-    checked.getAttribute("checked")
-        ? (checked.removeAttribute("checked"),
-          (checked.parentNode.parentNode.style.textDecoration = "none"))
-        : (checked.setAttribute("checked", true),
-          (checked.parentNode.parentNode.style.textDecoration =
-              "line-through"));
+﻿let globalID = 0;
+const todoList = document.getElementById("todoList");
+const addTitle = document.getElementById("addTitle");
+const addButton = document.getElementById("addButton");
+const todoTemplate = (title) => {
+    globalID++;
+    let todo = document.createElement("div");
+    todo.className = "todo";
+    todo.id = `todo${globalID}`;
+    todo.innerHTML = `
+    
+    <div class="title" id="title${globalID}">
+                              ${title}
+                      </div>
+                      <div class="checked">
+                          <input type="checkbox" id="checked${globalID}" oneclick="function(){console.log('1')}")">checked
+                      </div>
+                      <div class="edit">
+                          <button class="edit-button" id="edit${globalID}" oneclick="function(){console.log('1')}')}">edit</button>
+                      </div>
+                      <div class="delete">
+                          <button class="delete-button" id="delete${globalID}">delete</button>
+                      </div>`;
+    return todo;
+};
+addButton.addEventListener("click", () => {
+    if (addTitle.value !== "") {
+        console.log(addTitle.value);
+        todoList.prepend(todoTemplate(addTitle.value));
+        addTitle.value = "";
+    }
 });
 
+addTitle.addEventListener("keydown", (e) => {
+  if(e.key === "Enter"){
+    if (addTitle.value !== "") {
+      console.log(addTitle.value);
+      todoList.append(todoTemplate(addTitle.value));
+      addTitle.value = "";
+  }
+
+  }
+});
