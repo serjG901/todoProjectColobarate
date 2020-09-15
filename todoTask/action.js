8ae9b0bd6026d7a01155fea9fbc4106b196f827f
serjG901 <70537823+serjG901@users.noreply.github.com>
2020-09-15 12:38:47 +0300

    add todoTask folder

---
+++ todoTask/action.js
@@ -0,0 +1,11 @@
+﻿let checked = document.getElementById("checked");
+
+checked.addEventListener("change", () => {
+    checked.getAttribute("checked")
+        ? (checked.removeAttribute("checked"),
+          (checked.parentNode.parentNode.style.textDecoration = "none"))
+        : (checked.setAttribute("checked", true),
+          (checked.parentNode.parentNode.style.textDecoration =
+              "line-through"));
+});
+
